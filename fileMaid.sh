111d425213ba7ad29c0ed6c3fa3f9f280b4fbf5c
ntaveras <narayanataveras@gmail.com>
2023-05-11 22:16:13 -0400

    - added the Directory Tree creation - Began working on a method to get list of files in the Desktop

--- fileMaid.sh
+++ fileMaid.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+desktopDirectory=~/Desktop/
+
+[ -d "${desktopDirectory}" ] && echo "Directory exists." || createArchiveDirectoryTree
+
+function createArchiveDirectoryTree(){
+    mkdir -p "${desktopDirectory}/archive/Viewed_Less_Then_7_Days_Ago"
+    mkdir -p "${desktopDirectory}/archive/Viewed_Less_Then_30_Days_Ago"
+    mkdir -p "${desktopDirectory}/archive/Viewed_Over_Then_31_Days_Ago"
+    mkdir -p "${desktopDirectory}/archive/Purge_Candidates"
+}
+
+function doFileClean-upMaintenance(){
+echo getListOfFiles
+
+}
+
+function getListOfFiles(){
+for file in "${desktopDirectory}/*"
+do
+  echo "$entry"
+done
+}
+
+function updateFileName(){
+
+}
