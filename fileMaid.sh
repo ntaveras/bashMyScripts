3c8a62508cf96b52e5b173226b00fe9fb1ad3883
ntaveras <narayanataveras@gmail.com>
2023-05-11 22:27:44 -0400

    - Added description comment explaining the script and it's steps

--- fileMaid.sh
+++ fileMaid.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+<<commentBlock
+Description: 
+Creates an archived folder in the Desktop and organizes the files withing the folder tree by grouping them into: (1) Files opened in the last seven days [0 - 7 days], (2) Files opened within the las thirty days but not within the last 7 [8 - 30 days], (3) Files opened over thirty days ago [31 - 90 days]. 
 
-desktopDirectory=~/Desktop/
+Also renames the file to prepend the file name with the date of the last time it was accessed.
+commentBlock
 
+desktopDirectory=~/Desktop/ 
 [ -d "${desktopDirectory}" ] && echo "Directory exists." || createArchiveDirectoryTree
 
 function createArchiveDirectoryTree(){
